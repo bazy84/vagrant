4433676ba1a381fcb12596c294f47bdd1cdb4349
Nicolae Critu <nicolae.critu@unifiedpost.com>
2014-05-06 13:00:18 +0300

    provisioning by puppetmaster

--- provision_puppet.sh
+++ provision_puppet.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-set -x
+#set -x
 #
-puppet apply --verbose /vagrant/puppet/manifests/site.pp --modulepath=/vagrant/puppet/modules/
-
+#puppet apply --verbose /vagrant/puppet/manifests/site.pp --modulepath=/vagrant/puppet/modules/
+grep -q puppet /etc/hosts
+if [ "$?" -ne 0 ]; then
+  echo "192.168.42.99   puppet.test.lab" >> /etc/hosts
+fi
+puppet agent --verbose --debug --test --waitforcert=60 --server=puppet.test.lab
