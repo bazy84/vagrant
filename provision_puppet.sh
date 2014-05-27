7fd90964350db85f2851d3b7622c5eb88f5e1a5e
Nicolae Critu <nicolae.critu@unifiedpost.com>
2014-05-27 15:56:32 +0300

    back to setup without a puppetmaster

--- provision_puppet.sh
+++ provision_puppet.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 #set -x
 #
-#puppet apply --verbose /vagrant/puppet/manifests/site.pp --modulepath=/vagrant/puppet/modules/
-grep -q puppet /etc/hosts
-if [ "$?" -ne 0 ]; then
-  echo "192.168.42.99   puppet.test.lab" >> /etc/hosts
-fi
-puppet agent --verbose --debug --test --waitforcert=60 --server=puppet.test.lab
+puppet apply --verbose /vagrant/puppet/manifests/site.pp --modulepath=/vagrant/puppet/modules/
+
+#grep -q puppet /etc/hosts
+#if [ "$?" -ne 0 ]; then
+#  echo "192.168.42.99   puppet.test.lab" >> /etc/hosts
+#fi
+#puppet agent --verbose --debug --test --waitforcert=60 --server=puppet.test.lab
